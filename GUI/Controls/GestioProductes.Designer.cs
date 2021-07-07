d93c1b768d4e92e7a38bdbc79318c7096bda724a
joanp <none@none>
2021-07-07 11:45:59 +0200

    Afegit Traspàs en el filtre de productes.

--- GUI/Controls/GestioProductes.Designer.cs
+++ GUI/Controls/GestioProductes.Designer.cs
@@ -66,6 +66,13 @@ namespace Inversions.GUI
             this.ckAmbMoviments = new System.Windows.Forms.CheckBox();
             this.ckNomesAmbParticipacions = new System.Windows.Forms.CheckBox();
             this.pnFiltreAny = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.gbFiltreAny = new System.Windows.Forms.GroupBox();
+            this.cbFiltreAny = new System.Windows.Forms.ComboBox();
+            this.ckFiltreCompresAny = new System.Windows.Forms.CheckBox();
+            this.ckFiltreVendesAny = new System.Windows.Forms.CheckBox();
+            this.ckFiltreTraspasAny = new System.Windows.Forms.CheckBox();
+            this.ckFiltreDivAny = new System.Windows.Forms.CheckBox();
             this.pnSelDeselChecksProds = new System.Windows.Forms.Panel();
             this.btDeseleccionaTot = new System.Windows.Forms.Button();
             this.btSeleccionaTot = new System.Windows.Forms.Button();
@@ -80,13 +87,6 @@ namespace Inversions.GUI
             this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.cbTipusProducteFiltreTab2 = new System.Windows.Forms.ComboBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.ckFiltreVendesAny = new System.Windows.Forms.CheckBox();
-            this.cbFiltreAny = new System.Windows.Forms.ComboBox();
-            this.ckFiltreDivAny = new System.Windows.Forms.CheckBox();
-            this.gbFiltreAny = new System.Windows.Forms.GroupBox();
-            this.ckFiltreCompresAny = new System.Windows.Forms.CheckBox();
-            this.ckFiltreTraspasAny = new System.Windows.Forms.CheckBox();
             this.pnDadesProducte.SuspendLayout();
             this.panel1.SuspendLayout();
             this.pnDescripcioFons.SuspendLayout();
@@ -105,13 +105,13 @@ namespace Inversions.GUI
             this.gbFiltres.SuspendLayout();
             this.panel4.SuspendLayout();
             this.pnFiltreAny.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.gbFiltreAny.SuspendLayout();
             this.pnSelDeselChecksProds.SuspendLayout();
             this.panel3.SuspendLayout();
             this.gbFons.SuspendLayout();
             this.gbEmpresa.SuspendLayout();
             this.groupBox5.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
-            this.gbFiltreAny.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnDadesProducte
@@ -563,10 +563,10 @@ namespace Inversions.GUI
             // 
             // panel4
             // 
-            this.panel4.Controls.Add(this.ckAmbMoviments);
-            this.panel4.Controls.Add(this.ckNomesAmbParticipacions);
-            this.panel4.Controls.Add(this.pnFiltreAny);
             this.panel4.Controls.Add(this.pnSelDeselChecksProds);
+            this.panel4.Controls.Add(this.pnFiltreAny);
+            this.panel4.Controls.Add(this.ckNomesAmbParticipacions);
+            this.panel4.Controls.Add(this.ckAmbMoviments);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel4.Location = new System.Drawing.Point(3, 19);
             this.panel4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
@@ -581,10 +581,11 @@ namespace Inversions.GUI
             this.ckAmbMoviments.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.ckAmbMoviments.Checked = true;
             this.ckAmbMoviments.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckAmbMoviments.Dock = System.Windows.Forms.DockStyle.Left;
             this.ckAmbMoviments.Location = new System.Drawing.Point(3, 3);
             this.ckAmbMoviments.Margin = new System.Windows.Forms.Padding(0);
             this.ckAmbMoviments.Name = "ckAmbMoviments";
-            this.ckAmbMoviments.Size = new System.Drawing.Size(89, 65);
+            this.ckAmbMoviments.Size = new System.Drawing.Size(89, 74);
             this.ckAmbMoviments.TabIndex = 0;
             this.ckAmbMoviments.Text = "Amb\r\nMoviments";
             this.ckAmbMoviments.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -597,10 +598,11 @@ namespace Inversions.GUI
             this.ckNomesAmbParticipacions.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.ckNomesAmbParticipacions.Checked = true;
             this.ckNomesAmbParticipacions.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckNomesAmbParticipacions.Dock = System.Windows.Forms.DockStyle.Left;
             this.ckNomesAmbParticipacions.Location = new System.Drawing.Point(92, 3);
             this.ckNomesAmbParticipacions.Margin = new System.Windows.Forms.Padding(0);
             this.ckNomesAmbParticipacions.Name = "ckNomesAmbParticipacions";
-            this.ckNomesAmbParticipacions.Size = new System.Drawing.Size(111, 65);
+            this.ckNomesAmbParticipacions.Size = new System.Drawing.Size(111, 74);
             this.ckNomesAmbParticipacions.TabIndex = 1;
             this.ckNomesAmbParticipacions.Text = "Amb\r\nParticipacions";
             this.ckNomesAmbParticipacions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -608,26 +610,121 @@ namespace Inversions.GUI
             // 
             // pnFiltreAny
             // 
+            this.pnFiltreAny.AutoSize = true;
+            this.pnFiltreAny.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.pnFiltreAny.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnFiltreAny.Controls.Add(this.flowLayoutPanel1);
+            this.pnFiltreAny.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnFiltreAny.Location = new System.Drawing.Point(203, 3);
             this.pnFiltreAny.Margin = new System.Windows.Forms.Padding(0);
             this.pnFiltreAny.Name = "pnFiltreAny";
-            this.pnFiltreAny.Size = new System.Drawing.Size(345, 72);
+            this.pnFiltreAny.Size = new System.Drawing.Size(335, 74);
             this.pnFiltreAny.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this.gbFiltreAny);
+            this.flowLayoutPanel1.Controls.Add(this.ckFiltreCompresAny);
+            this.flowLayoutPanel1.Controls.Add(this.ckFiltreVendesAny);
+            this.flowLayoutPanel1.Controls.Add(this.ckFiltreTraspasAny);
+            this.flowLayoutPanel1.Controls.Add(this.ckFiltreDivAny);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(333, 72);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // gbFiltreAny
+            // 
+            this.gbFiltreAny.Controls.Add(this.cbFiltreAny);
+            this.gbFiltreAny.Location = new System.Drawing.Point(5, 10);
+            this.gbFiltreAny.Margin = new System.Windows.Forms.Padding(5, 10, 0, 0);
+            this.gbFiltreAny.Name = "gbFiltreAny";
+            this.gbFiltreAny.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gbFiltreAny.Size = new System.Drawing.Size(78, 54);
+            this.gbFiltreAny.TabIndex = 6;
+            this.gbFiltreAny.TabStop = false;
+            this.gbFiltreAny.Text = "Any";
+            // 
+            // cbFiltreAny
+            // 
+            this.cbFiltreAny.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbFiltreAny.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFiltreAny.Enabled = false;
+            this.cbFiltreAny.FormattingEnabled = true;
+            this.cbFiltreAny.Location = new System.Drawing.Point(3, 23);
+            this.cbFiltreAny.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbFiltreAny.Name = "cbFiltreAny";
+            this.cbFiltreAny.Size = new System.Drawing.Size(72, 28);
+            this.cbFiltreAny.TabIndex = 0;
+            // 
+            // ckFiltreCompresAny
+            // 
+            this.ckFiltreCompresAny.AutoSize = true;
+            this.ckFiltreCompresAny.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ckFiltreCompresAny.Location = new System.Drawing.Point(83, 10);
+            this.ckFiltreCompresAny.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.ckFiltreCompresAny.Name = "ckFiltreCompresAny";
+            this.ckFiltreCompresAny.Size = new System.Drawing.Size(77, 45);
+            this.ckFiltreCompresAny.TabIndex = 4;
+            this.ckFiltreCompresAny.Text = "Compres";
+            this.ckFiltreCompresAny.UseVisualStyleBackColor = true;
+            this.ckFiltreCompresAny.CheckedChanged += new System.EventHandler(this.ckFiltreAny_CheckedChanged);
+            // 
+            // ckFiltreVendesAny
+            // 
+            this.ckFiltreVendesAny.AutoSize = true;
+            this.ckFiltreVendesAny.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ckFiltreVendesAny.Location = new System.Drawing.Point(160, 10);
+            this.ckFiltreVendesAny.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.ckFiltreVendesAny.Name = "ckFiltreVendesAny";
+            this.ckFiltreVendesAny.Size = new System.Drawing.Size(68, 45);
+            this.ckFiltreVendesAny.TabIndex = 5;
+            this.ckFiltreVendesAny.Text = "Vendes";
+            this.ckFiltreVendesAny.UseVisualStyleBackColor = true;
+            this.ckFiltreVendesAny.CheckedChanged += new System.EventHandler(this.ckFiltreAny_CheckedChanged);
+            // 
+            // ckFiltreTraspasAny
+            // 
+            this.ckFiltreTraspasAny.AutoSize = true;
+            this.ckFiltreTraspasAny.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ckFiltreTraspasAny.Enabled = false;
+            this.ckFiltreTraspasAny.Location = new System.Drawing.Point(228, 10);
+            this.ckFiltreTraspasAny.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.ckFiltreTraspasAny.Name = "ckFiltreTraspasAny";
+            this.ckFiltreTraspasAny.Size = new System.Drawing.Size(70, 45);
+            this.ckFiltreTraspasAny.TabIndex = 8;
+            this.ckFiltreTraspasAny.Text = "Traspàs";
+            this.ckFiltreTraspasAny.UseVisualStyleBackColor = true;
+            this.ckFiltreTraspasAny.CheckedChanged += new System.EventHandler(this.ckFiltreAny_CheckedChanged);
+            // 
+            // ckFiltreDivAny
+            // 
+            this.ckFiltreDivAny.AutoSize = true;
+            this.ckFiltreDivAny.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.ckFiltreDivAny.Location = new System.Drawing.Point(298, 10);
+            this.ckFiltreDivAny.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.ckFiltreDivAny.Name = "ckFiltreDivAny";
+            this.ckFiltreDivAny.Size = new System.Drawing.Size(35, 45);
+            this.ckFiltreDivAny.TabIndex = 7;
+            this.ckFiltreDivAny.Text = "Div";
+            this.ckFiltreDivAny.UseVisualStyleBackColor = true;
+            this.ckFiltreDivAny.CheckedChanged += new System.EventHandler(this.ckFiltreAny_CheckedChanged);
             // 
             // pnSelDeselChecksProds
             // 
-            this.pnSelDeselChecksProds.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pnSelDeselChecksProds.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.pnSelDeselChecksProds.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnSelDeselChecksProds.Controls.Add(this.btDeseleccionaTot);
             this.pnSelDeselChecksProds.Controls.Add(this.btSeleccionaTot);
-            this.pnSelDeselChecksProds.Location = new System.Drawing.Point(482, 3);
+            this.pnSelDeselChecksProds.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnSelDeselChecksProds.Location = new System.Drawing.Point(538, 3);
             this.pnSelDeselChecksProds.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
             this.pnSelDeselChecksProds.Name = "pnSelDeselChecksProds";
             this.pnSelDeselChecksProds.Padding = new System.Windows.Forms.Padding(3);
-            this.pnSelDeselChecksProds.Size = new System.Drawing.Size(208, 72);
+            this.pnSelDeselChecksProds.Size = new System.Drawing.Size(208, 74);
             this.pnSelDeselChecksProds.TabIndex = 3;
             this.pnSelDeselChecksProds.Visible = false;
             // 
@@ -766,96 +863,6 @@ namespace Inversions.GUI
             this.cbTipusProducteFiltreTab2.TabIndex = 0;
             this.cbTipusProducteFiltreTab2.SelectedIndexChanged += new System.EventHandler(this.cbTipusProducteFiltreTab2_SelectedIndexChanged);
             // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Controls.Add(this.gbFiltreAny);
-            this.flowLayoutPanel1.Controls.Add(this.ckFiltreCompresAny);
-            this.flowLayoutPanel1.Controls.Add(this.ckFiltreVendesAny);
-            this.flowLayoutPanel1.Controls.Add(this.ckFiltreTraspasAny);
-            this.flowLayoutPanel1.Controls.Add(this.ckFiltreDivAny);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(343, 70);
-            this.flowLayoutPanel1.TabIndex = 0;
-            // 
-            // ckFiltreVendesAny
-            // 
-            this.ckFiltreVendesAny.AutoSize = true;
-            this.ckFiltreVendesAny.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.ckFiltreVendesAny.Location = new System.Drawing.Point(160, 10);
-            this.ckFiltreVendesAny.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
-            this.ckFiltreVendesAny.Name = "ckFiltreVendesAny";
-            this.ckFiltreVendesAny.Size = new System.Drawing.Size(68, 45);
-            this.ckFiltreVendesAny.TabIndex = 5;
-            this.ckFiltreVendesAny.Text = "Vendes";
-            this.ckFiltreVendesAny.UseVisualStyleBackColor = true;
-            this.ckFiltreVendesAny.CheckedChanged += new System.EventHandler(this.ckFiltreAny_CheckedChanged);
-            // 
-            // cbFiltreAny
-            // 
-            this.cbFiltreAny.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cbFiltreAny.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbFiltreAny.Enabled = false;
-            this.cbFiltreAny.FormattingEnabled = true;
-            this.cbFiltreAny.Location = new System.Drawing.Point(3, 23);
-            this.cbFiltreAny.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.cbFiltreAny.Name = "cbFiltreAny";
-            this.cbFiltreAny.Size = new System.Drawing.Size(72, 28);
-            this.cbFiltreAny.TabIndex = 0;
-            // 
-            // ckFiltreDivAny
-            // 
-            this.ckFiltreDivAny.AutoSize = true;
-            this.ckFiltreDivAny.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.ckFiltreDivAny.Location = new System.Drawing.Point(298, 10);
-            this.ckFiltreDivAny.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
-            this.ckFiltreDivAny.Name = "ckFiltreDivAny";
-            this.ckFiltreDivAny.Size = new System.Drawing.Size(35, 45);
-            this.ckFiltreDivAny.TabIndex = 7;
-            this.ckFiltreDivAny.Text = "Div";
-            this.ckFiltreDivAny.UseVisualStyleBackColor = true;
-            this.ckFiltreDivAny.CheckedChanged += new System.EventHandler(this.ckFiltreAny_CheckedChanged);
-            // 
-            // gbFiltreAny
-            // 
-            this.gbFiltreAny.Controls.Add(this.cbFiltreAny);
-            this.gbFiltreAny.Location = new System.Drawing.Point(5, 10);
-            this.gbFiltreAny.Margin = new System.Windows.Forms.Padding(5, 10, 0, 0);
-            this.gbFiltreAny.Name = "gbFiltreAny";
-            this.gbFiltreAny.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.gbFiltreAny.Size = new System.Drawing.Size(78, 54);
-            this.gbFiltreAny.TabIndex = 6;
-            this.gbFiltreAny.TabStop = false;
-            this.gbFiltreAny.Text = "Any";
-            // 
-            // ckFiltreCompresAny
-            // 
-            this.ckFiltreCompresAny.AutoSize = true;
-            this.ckFiltreCompresAny.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.ckFiltreCompresAny.Location = new System.Drawing.Point(83, 10);
-            this.ckFiltreCompresAny.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
-            this.ckFiltreCompresAny.Name = "ckFiltreCompresAny";
-            this.ckFiltreCompresAny.Size = new System.Drawing.Size(77, 45);
-            this.ckFiltreCompresAny.TabIndex = 4;
-            this.ckFiltreCompresAny.Text = "Compres";
-            this.ckFiltreCompresAny.UseVisualStyleBackColor = true;
-            this.ckFiltreCompresAny.CheckedChanged += new System.EventHandler(this.ckFiltreAny_CheckedChanged);
-            // 
-            // ckFiltreTraspasAny
-            // 
-            this.ckFiltreTraspasAny.AutoSize = true;
-            this.ckFiltreTraspasAny.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.ckFiltreTraspasAny.Enabled = false;
-            this.ckFiltreTraspasAny.Location = new System.Drawing.Point(228, 10);
-            this.ckFiltreTraspasAny.Margin = new System.Windows.Forms.Padding(0, 10, 0, 0);
-            this.ckFiltreTraspasAny.Name = "ckFiltreTraspasAny";
-            this.ckFiltreTraspasAny.Size = new System.Drawing.Size(70, 45);
-            this.ckFiltreTraspasAny.TabIndex = 8;
-            this.ckFiltreTraspasAny.Text = "Traspàs";
-            this.ckFiltreTraspasAny.UseVisualStyleBackColor = true;
-            this.ckFiltreTraspasAny.CheckedChanged += new System.EventHandler(this.ckFiltreAny_CheckedChanged);
-            // 
             // GestioProductes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -894,14 +901,15 @@ namespace Inversions.GUI
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.pnFiltreAny.ResumeLayout(false);
+            this.pnFiltreAny.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.gbFiltreAny.ResumeLayout(false);
             this.pnSelDeselChecksProds.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.gbFons.ResumeLayout(false);
             this.gbEmpresa.ResumeLayout(false);
             this.groupBox5.ResumeLayout(false);
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.PerformLayout();
-            this.gbFiltreAny.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
