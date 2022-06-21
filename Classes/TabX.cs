13139cb96f03b57a8b41d89242bac21268645799
joanp <none@none>
2022-06-21 13:51:48 +0200

    Substitueixo iTabs per TabX

---
+++ Classes/TabX.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Inversions.GUI;
+
+namespace Inversions
+{
+    public class TabX : UserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private readonly IContainer components = null;
+
+        protected TabX()
+        {
+            // Required method for Designer support - do not modify 
+            // the contents of this method with the code editor.
+            components = new Container();
+            AutoScaleMode = AutoScaleMode.Font;
+        }
+
+        /// <summary>
+        /// Indica que la pestanya està s'està editant.
+        /// </summary>
+        protected bool _EnModeEdicio { get; set; }
+
+        /// <summary>
+        /// Indica que s'han de recarregar les dades de la pestanya
+        /// </summary>
+        internal bool _ActivaRefresca { get; set; }
+
+        /// <summary>
+        /// Si true, indica que s'han de carregar les dades de la pestanya per primer cop.
+        /// </summary>
+        internal bool _CarregaDadesInicial { get; set; }
+
+
+        protected void acceptButton(Button botoAccept)
+        {
+            if (ParentForm != null) 
+                ParentForm.AcceptButton = botoAccept;
+        }
+
+        protected void cancelButton(Button botoCancel)
+        {
+            if (ParentForm != null)
+                ParentForm.CancelButton = botoCancel;
+        }
+
+        /// <summary>
+        /// Refresca les dades de la pestanya.
+        /// </summary>
+        /// <param name="refrescaActivat"> Si no null canvia el valor de 'activaRefresca' en la pestanya seleccionada.</param>
+        internal virtual void refresca(bool? refrescaActivat)
+        {
+            if (refrescaActivat.HasValue)
+                _ActivaRefresca = refrescaActivat.Value;
+        }
+
+        /// <summary>
+        /// Quan Principal detecta canvi d'usuari, crida el mètode de la pestanya seleccionada.
+        /// </summary>
+        /// <param name="usuari">Usuari seleccionat</param>
+        public virtual void canviUsuari(Usuari usuari) { }
+
+        /// <summary>
+        /// Quan Principal detecta Escape, crida el mètode de la pestanya seleccionada.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public virtual void escape(object sender, KeyEventArgs e) { }
+
+        protected virtual void modeEdicio()
+        {
+            _EnModeEdicio = true;
+        }
+
+        protected virtual void modeConsulta()
+        {
+            _EnModeEdicio = false;
+        }
+
+        /// <summary>
+        /// Impideix sortir de la pastanya si està en mode edició.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        internal void validating(object sender, CancelEventArgs e)
+        {
+            if (_EnModeEdicio)
+            {
+                MessageBox.Show("Està en mode edició");
+                e.Cancel = true;
+            }
+        }
+
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
